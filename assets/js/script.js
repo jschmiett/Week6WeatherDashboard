37e21699067aa87cd50ac95f179720e50e50b22b
Jennifer Schmiett <bailliejm@gmail.com>
2022-04-23 21:40:05 -0500

    Getting API working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 console.log("hello world")
+
+
+
+var APIkey = "16614a127342a5014cfcb8d183881e22";
+
+var city = window.prompt("What city would you like to see the weather for?");
+console.log("city: " + city);
+
+queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=" + APIkey;
+
+var weather = fetch(queryURL);
+console.log(weather);
+
