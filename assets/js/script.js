03a53107cb933483fec7fd6ab3bc5adefe70c417
Jennifer Schmiett <bailliejm@gmail.com>
2022-08-16 15:07:26 -0500

    Basic html framework for submit button and linking script.js to index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
 console.log("hello world")
-
-
+var submitButton = document.getElementById('cityWeatherSearch');
 
 var APIkey = "16614a127342a5014cfcb8d183881e22";
+console.log(APIkey);
 
-var city = window.prompt("What city would you like to see the weather for?");
-console.log("city: " + city);
+var city = document.getElementById('cityWeatherSearch');
+console.log(city);
 
-queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=" + APIkey;
 
-var weather = fetch(queryURL);
-console.log(weather);
+function getCityWeather() {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+    console.log(queryURL);
 
+    fetch(queryURL)
+        .then(function (response) {
+            console.log(response)
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+
+}
